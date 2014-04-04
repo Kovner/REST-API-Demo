63369db5a89a4afef42a7ee0c77ae1ef834aa1a2
Mike Kovner <mike.kovner@gmail.com>
2014-04-04 09:06:57 -0700

    Adds functionality for making calls to Server

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+var XMLWriter = require('xml-writer');
+var pd = require("pretty-data").pd;
+var request = require("request");
 var express = require("express");
 var app = express();
 
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
+app.use(express.urlencoded());
 app.use(express.static(__dirname + '/public'));
 app.use(express.cookieParser('Ronaldinho'));
 app.use(express.session());
@@ -15,11 +19,27 @@ app.get('/', function(req,res) {
 });
 
 app.post('/login', function(req,res) {
-	req.session.reqTxt = "Hello";
-	req.session.resTxt = "World";
-	res.redirect('/');
+	var reqxml = new XMLWriter();
+	reqxml.startElement('tsRequest').startElement('credentials').writeAttribute('name', req.body.Username)
+		.writeAttribute('password', req.body.Pass).startElement('site').writeAttribute('contentUrl', '');
+	req.session.reqTxt = pd.xml(reqxml.toString());
+	request.post(
+		{
+			url: 'http://localhost/api/2.0/auth/signin',
+			body: reqxml.toString(),
+			headers: {'Content-Type': 'text/xml'}
+		},
+		function(err, response, body) {
+			if(err) {
+				req.session.resTxt = err;
+			} else {
+				req.session.resTxt = body;
+			}
+			res.redirect('/');
+		}
+	);
 });
 
-var port = Number(process.env.PORT || 8888);
+var port = Number(process.env.PORT || 8000);
 app.listen(port);
 console.log("Listening on port " + port);
